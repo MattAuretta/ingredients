999cdbc94b3f7839bebac2e7c65717690266dc29
MattAuretta <mauretta2@gmail.com>
2020-01-17 14:03:35 -0700

    error handling and using loadingState

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,28 +2,13 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
   const [ingredientsState, setIngredientsState] = useState([]);
-
-  // This is no longer needed, since Search.js sets our state with props.onLoadIngredients
-  // useEffect(() => {
-  //   fetch('https://react-hooks-1d6ad.firebaseio.com/ingredients.json')
-  //     .then(response => response.json())
-  //     .then(responseData => {
-  //       console.log(responseData);
-  //       const loadedIngredients = [];
-  //       for (let key in responseData) {
-  //         loadedIngredients.push({
-  //           id: key,
-  //           title: responseData[key].title,
-  //           amount: responseData[key].amount
-  //         });
-  //       }
-  //       setIngredientsState(loadedIngredients);
-  //     });
-  // }, []);
+  const [loadingState, setLoadingState] = useState(false);
+  const [errorState, setErrorState] = useState(null);
 
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', ingredientsState)
@@ -34,32 +19,56 @@ const Ingredients = () => {
   }, []);
 
   const addIngredientHandler = (ingredient) => {
+    setLoadingState(true);
     fetch('https://react-hooks-1d6ad.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     })
-    .then(response => response.json())
-    .then(responseData => {
-      setIngredientsState(prevIngredients => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient }
-      ]);
-    });
+      .then(response => {
+        setLoadingState(false);
+        return response.json();
+      })
+      .then(responseData => {
+        setIngredientsState(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      })
+      .catch(error => {
+        // Both state updates are batched together and executed synchronously in one render cycle.
+        setErrorState(error.message);
+        setLoadingState(false);
+      });
   }
 
   const removeIngredientHandler = (id) => {
-    fetch(`https://react-hooks-1d6ad.firebaseio.com/ingredients/${id}.json`, {
+    setLoadingState(true);
+    fetch(`https://react-hooks-1d6ad.firebaseio.com/ingredients/${id}.jon`, {
       method: 'DELETE'
     })
-    .then(response => {
-      setIngredientsState(prevIngredients => prevIngredients.filter(item => item.id !== id))
-    });
+      .then(response => {
+        setLoadingState(false);
+        setIngredientsState(prevIngredients => prevIngredients.filter(item => item.id !== id))
+      })
+      .catch(error => {
+        // Both state updates are batched together and executed synchronously in one render cycle.
+        setErrorState(error.message);
+        setLoadingState(false);
+      });
+  }
+
+  const resetErrorMessageHandler = () => {
+    setErrorState(null);
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {errorState && <ErrorModal onClose={resetErrorMessageHandler}>{errorState}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={loadingState}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
